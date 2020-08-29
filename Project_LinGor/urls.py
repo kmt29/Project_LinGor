132112a423d2b4db12ecd111f7c26bf32c009bd7
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2020-08-29 19:40:13 +0630

    added vir-env, home api, about api

--- Project_LinGor/urls.py
+++ Project_LinGor/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include("shop.urls")),
-    path('',include("index.urls")),
+    path('', include("index.urls")),
 ]
